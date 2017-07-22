99d029262f6682bbbfed994c99a40128e5fa47b6
Aleksandr Ignatyev <vigntom.2708@gmail.com>
2017-07-22 13:06:22 +0300

    Create basic functionality

---
+++ index.js
@@ -0,0 +1,10 @@
+const app = require('./lib/server')
+
+const port = process.env.PORT || 5000
+
+function error(e, socket) {
+  console.warn('Error: ', e.message)
+  socket.end('HTTP/1.1 400 Bad Request\r\n\r\n')
+}
+
+app.server.on('clentError', error).listen(port)
